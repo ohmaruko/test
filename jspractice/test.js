14e3eecfeb76ab361d2e34c1bcf0448482abe9aa
ohmaruko <mnogaki@my.bcit.ca>
2023-09-09 23:30:22 -0700

    update test file, add test2 file

--- jspractice/test.js
+++ jspractice/test.js
@@ -5,14 +5,15 @@ console.log("Hello World");
  * 
  * 
  */
-var apple=1;
+var apple = 1;
+grape = 2;
 
-var number =1;
+var number = 1;
 console.log(number);
 number = 3;
 
 
 var x = 10;
-var y =5;
+var y = 5;
 var z = x + y;
 console.log(z);
