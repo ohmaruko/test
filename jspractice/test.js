fe8a2a79834d87b6298d3ded1ee5ae341ba00cae
MADOKA <MADOKA@nokakienges-MacBook-Air.local>
2023-09-09 23:06:34 -0700

    adding test1 file

---
+++ jspractice/test.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+//alealealeal
+/**
+ * 
+ * 
+ * 
+ */
+var apple=1;
+
+var number =1;
+console.log(number);
+number = 3;
+
+
+var x = 10;
+var y =5;
+var z = x + y;
+console.log(z);
