1086c0a29a2c016695c3955a5f5e0e0857d232a0
madoka <mnogaki@my.bcit.ca>
2023-09-10 22:54:20 -0700

    update test2 file

---
+++ jspractice/test2.js
@@ -0,0 +1,48 @@
+//aaaaa
+/**
+ * practice using workbooklet
+ */
+
+//booklet 2.1 camel case
+let numberCables = 22;
+let electronicDevices = 5;
+let schoolSupplies = 34;
+let vstudyHours = 8;
+let powerPacks = 72;
+//booklet 2.2 pascal case
+let NumberCables = 22;
+let ElectronicDevices = 5;
+let SchoolSupplies = 34;
+let StudyHours = 8;
+let PowerPacks = 72;
+//booklet 2.3 snake case
+let number_cables = 22;
+let electronic_devices = 5;
+let school_supplies = 34;
+let study_hours = 8;
+let power_packs = 72;
+
+/**number = ;
+*baseball = ;
+*car = ;
+*school = ;
+*/
+//declare
+let number;
+let baseball;
+let car;
+let school;
+//initialize
+seeds = 3;
+oranges = 10;
+laptops = 1;
+fan = 40;
+speakers = 22;
+//declare then assign
+let seeds = 3;
+let oranges = 10;
+let laptops = 1;
+let fan = 40;
+let speakers = 22;
+
+
